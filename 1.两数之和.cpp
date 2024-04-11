6cb65ddac659bfc3f42159fff795640e84e83a07
2874160799 <anyuehuanye@qq.com>
2024-04-11 11:11:01 +0800

    first commit

---
+++ 1.两数之和.cpp
@@ -0,0 +1,37 @@
+// @before-stub-for-debug-begin
+#include <vector>
+#include <string>
+#include "commoncppproblem1.h"
+
+using namespace std;
+// @before-stub-for-debug-end
+
+/*
+ * @lc app=leetcode.cn id=1 lang=cpp
+ *
+ * [1] 两数之和
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int>v1;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            for (int j = i+1; j < nums.size(); j++)
+            {
+                if(nums[i]+nums[j] == target)
+                {
+                    v1.push_back(i);
+                    v1.push_back(j);
+                }
+            }
+            
+        }
+        
+        return v1;
+    }
+};
+// @lc code=end
+
